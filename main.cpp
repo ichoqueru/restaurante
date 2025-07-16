e29922323fef616bcf739315403c9f3098d15a21
ichoqueru <ichoquer@unjbg.edu.pe>
2025-07-16 08:42:25 -0500

    Añade la modularización del código

--- main.cpp
+++ main.cpp
@@ -1,48 +1,9 @@
 #include<iostream>
 #include<string>
 #include<stdlib.h>
+#include"menu.h"
+#include"cliente.h"
 using namespace std;
-
-struct pedido{
-	string tipo;
-	string nombre;
-	float precio;
-	int cantidad;
-};
-
-struct cliente{
-    string nom;
-    int mesa;    
-    float monto;
-    int cantPedidos;
-    struct pedido pedidos[20];
-}mesas[200];
-
-struct menu{
-    string bebidas[10];
-    float preciosBebidas[10];
-	string platos[10];
-	float preciosPlatos[10];
-	string postres[10];
-	float preciosPostres[10];
-    int cantB,cantP,cantC;
-};
-
-struct info_delivery{
-	string direct,nomd;
-	int telefono,dni;
-}entregas[200];
-
-struct factura{
-	int d,s,a,opc,cantdeli,cant;
-	struct info_delivery entregas[200];
-	struct cliente mesas[200];
-};
-
-
-void imprimeMenu(menu &);
-void imprimeFactura(factura &);
-void realizarPedido(menu &, cliente &);
 
 int main(){
     int op,comida,pedido,eleccion;
@@ -65,6 +26,8 @@ int main(){
 	string nombre;
 	float precio;
 	int cantidad;
+    struct cliente mesas[200];
+    struct info_delivery entregas[200];
 
     do{
         system("cls");  
@@ -209,147 +172,3 @@ int main(){
     return 0;
 }
 
-
-void imprimeMenu(menu &m){
-	cout<<"----------------"<<endl<<"CARTA DEL DIA"<<endl<<"----------------"<<endl;
-	
-	if(m.cantB>0){
-		cout<<"---BEBIDAS---"<<endl;
-    	for(int i=0;i<m.cantB;i++){
-    	   cout<<"Bebida "<<i+1<<": "<<m.bebidas[i]<<" - S/."<<m.preciosBebidas[i]<<endl;
-		}
-    }
-    
-    if(m.cantC>0){
-    	cout<<"\n---PLATO DE FONDO---"<<endl;
-        for(int i=0;i<m.cantC;i++){
-            cout<<"Platillo "<<i+1<<": "<<m.platos[i]<<" - S/."<<m.preciosPlatos[i]<<endl;
-        }          
-    }
-    
-    if(m.cantP>0){
-    	cout<<"\n---POSTRES---"<<endl;
-        for(int i=0;i<m.cantP;i++){
-            cout<<"Postres "<<i+1<<": "<<m.postres[i]<<" - S/."<<m.preciosPostres[i]<<endl;
-        }          
-    }
-        
-}
-
-void imprimeFactura(factura &f){
-	system("cls");
-	cout<<"----------------"<<endl<<"BOLETA"<<endl<<"----------------"<<endl;
-	cout<<"\n";
-	
-	if(f.opc==1){
-		cout<<"FECHA: "<<f.d<<"/"<<f.s<<"/"<<f.a<<endl;
-	       for(int i=0;i<f.cant;i++){           	           
-              cout<<"Nombre del cliente: "<<f.mesas[i].nom<<endl;
-              cout<<"Mesa: "<<f.mesas[i].mesa<<endl;      
-			
-			  cout<<"\n---PEDIDOS---"<<endl;  //muestra el pedido
-              for(int j=0; j<f.mesas[i].cantPedidos; j++){
-                cout<<f.mesas[i].pedidos[j].cantidad<<" x "<<f.mesas[i].pedidos[j].nombre;
-                cout<<" (S/."<<f.mesas[i].pedidos[j].precio<<") = ";
-                cout<<"S/."<<f.mesas[i].pedidos[j].precio * f.mesas[i].pedidos[j].cantidad<<endl;
-              }
-              cout<<"TOTAL: S/."<<f.mesas[i].monto<<endl;
-              cout<<"\n";  
-              cout<<"----------------------------------------------------------------"<<endl;
-           }		
-	}
-	else{
-	     if(f.opc==2){
-         cout<<"FECHA: "<<f.d<<"/"<<f.s<<"/"<<f.a<<endl;
-            for(int i=0;i<f.cantdeli;i++){
-                cout<<"Nombre del cliente: "<<f.entregas[i].nomd<<endl;
-                cout<<"Direccion: "<<f.entregas[i].direct<<endl;
-                cout<<"Ingrese el DNI: "<<f.entregas[i].dni<<endl;
-                cout<<"Ingrese el telefono: "<<f.entregas[i].telefono<<endl;	
-				
-			    cout<<"\n---PEDIDO---"<<endl;   //muestra el pedido
-                for(int j=0; j<f.mesas[i].cantPedidos; j++){
-                   cout<<f.mesas[i].pedidos[j].cantidad<<" x "<<f.mesas[i].pedidos[j].nombre;
-                   cout<<" (S/."<<f.mesas[i].pedidos[j].precio<<") = ";
-                   cout<<"S/."<<f.mesas[i].pedidos[j].precio * f.mesas[i].pedidos[j].cantidad<<endl;
-                }
-                cout<<"TOTAL: S/."<<f.mesas[i].monto<<endl;
-                cout<<"\n";		
-                cout<<"----------------------------------------------------------------"<<endl;
-	        } 
-         }
-    }
-}
-
-void realizarPedido(menu &m, cliente &c){
-	int opn,cantidad,item;
-	do{
-		system("cls");
-		imprimeMenu(m);  //muetsra el menu al cliente
-		cout<<"\n-----------------------------------------------------------------"<<endl;
-		cout<<"1. Agregar bebida"<<endl;
-		cout<<"2. Agregar plato"<<endl;
-		cout<<"3. Agregar postre"<<endl;
-		cout<<"4. Finalizar pedido"<<endl;
-		cout<<"Seleccione opcion: "; cin>>opn;
-		
-		switch(opn){
-			case 1: //bebidas
-			    if(m.cantB>0){
-				     cout<<"Seleccione bebida (1-"<<m.cantB<<"): ";
-				     cin>>item;
-				     cout<<"Cantidad a pedir: ";
-				     cin>>cantidad;
-				
-				    c.pedidos[c.cantPedidos].tipo="bebida";
-				    c.pedidos[c.cantPedidos].nombre=m.bebidas[item-1];
-				    c.pedidos[c.cantPedidos].precio=m.preciosBebidas[item-1];
-				    c.pedidos[c.cantPedidos].cantidad=cantidad;
-				    c.cantPedidos++;
-			    }
-			system("pause");
-			break;
-			
-			case 2:    //platos
-				if(m.cantC>0){
-				     cout<<"Seleccione platillo (1-"<<m.cantC<<"): ";
-				     cin>>item;
-				     cout<<"Cantidad a pedir: ";
-				     cin>>cantidad;
-				
-				    c.pedidos[c.cantPedidos].tipo="plato";
-				    c.pedidos[c.cantPedidos].nombre=m.platos[item-1];
-				    c.pedidos[c.cantPedidos].precio=m.preciosPlatos[item-1];
-				    c.pedidos[c.cantPedidos].cantidad=cantidad;				    
-				    c.cantPedidos++;
-			    }
-				system("pause");
-				break;
-				
-			case 3:    //postres
-				if(m.cantP>0){
-				     cout<<"Seleccione postre (1-"<<m.cantP<<"): ";
-				     cin>>item;
-				     cout<<"Cantidad a pedir: ";
-				     cin>>cantidad;
-				
-				    c.pedidos[c.cantPedidos].tipo="postre";
-				    c.pedidos[c.cantPedidos].nombre=m.postres[item-1];
-				    c.pedidos[c.cantPedidos].precio=m.preciosPostres[item-1];
-				    c.pedidos[c.cantPedidos].cantidad=cantidad;
-				    c.cantPedidos++;
-			    }
-				system("pause");
-				break;
-					
-			case 4: //calcular el total y finaliza el pedido 
-				for(int i=0;i<c.cantPedidos;i++){
-			            c.monto+=c.pedidos[i].precio*c.pedidos[i].cantidad;
-		        }		        
-		        
-		    break;
-		}
-		
-	}while(opn!=4);	
-
-}
